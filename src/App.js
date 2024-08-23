996929ddb2959246c2036b6da4a7a14a0429eb46
thiago-ulloa-develcode <84195990+thiagoulloa@users.noreply.github.com>
2024-08-23 10:27:59 -0300

    adding components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import ListaProdutos from './Componentes/ListaProdutos';
+
+const produtos = [{
+  nome:"Roblox",
+  preco: 29.99,
+  },
+  {
+    nome:"Minecraft",
+    preco: 60.00,
+    },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Lista Produtos</h1>
+    <ListaProdutos produtos={produtos}/>
+  </div>
   );
 }
 
---
+++ src/Componentes/ListaProdutos/index.js
@@ -0,0 +1,12 @@
+import Produto from "../Produto";
+
+const ListaProdutos = ({ produtos }) => { 
+        return (
+            <div> {produtos.map(
+                (p)=> (<Produto valor={p}></Produto>)
+            )}
+            </div>
+        );
+    }
+
+    export default ListaProdutos;
